8a19f20a32126f0893557883fb81b42dc4e6109f
ganeshbahire01 <ganesh12345bahire@gmail.com>
2023-04-06 13:03:14 +0530

    agian

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
 
-const connection = mongoose.connect(process.env.MONGODB)
+const connection = mongoose.connect("mongodb+srv://ganesh:ganesh@cluster0.6d4ur0f.mongodb.net/mock?retryWrites=true&w=majority")
 
 module.exports = connection
